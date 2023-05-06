c66f8d917614931a7f0eb2a1b3a808727efd57ab
Jonathan Felicity <jonathanfelicity@mail.com>
2023-05-06 20:57:14 +0100

    learn annotating object

---
+++ 3 objects/obj.ts
@@ -0,0 +1,12 @@
+const users:{
+    id: number,
+    email: string,
+    status?: string // this will make status optional 
+} = {
+    id: 1,
+    email: "f@fake.com"
+}
+
+
+// storing arrays of objects
+let accounts: {}[];
